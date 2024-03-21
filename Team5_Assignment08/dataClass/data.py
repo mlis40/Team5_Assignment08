db2914d973748c3adff2f2143c18c5aabcfbea08
mliso <mliso@DESKTOP-V75G00O.wifi.uc.edu>
2024-03-21 09:06:48 -0400

    Added Packages

---
+++ Team5_Assignment08/dataClass/data.py
@@ -0,0 +1,20 @@
+# dataClass.oy
+# The Data Class
+
+# imports
+import pyodbc
+
+class Data:
+    def Connect(self):
+        '''
+        Connect to the database and create a cursor
+        @return: the cursor object
+        '''
+        conn = pyodbc.connect('Driver={SQL Server};'
+                              'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                              'Database=GroceryStoreSimulator;'
+                              'uid=IS4010Login;'
+                              'pwd=P@ssword2;')
+        
+        cursor = conn.cursor()
+        return cursor
---
+++ Team5_Assignment08/mainPackage/main.py
@@ -0,0 +1,23 @@
+# main.py
+# Name: Matthew Lisowsky
+# email: lisowsmd@mail.uc.edu
+# Assignment Number: InClass_20240319
+# Due Date: 
+# Course/Section: IS4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+# Citations:
+# Anything else that's relevant:
+
+# imports (ex: from functionPackage.functions import *)
+from dataClassPackage.dataClass import Data
+
+if __name__ == "__main__": 
+    '''
+    # Instantiate an object of type Data
+    myData =  Data()
+    # Invoke the connect method and store what it returns in another variable
+    myCursor = myData.Connect()
+    
+    myCursor.execute() # Submit a query to the SQL Server instance and store the results in the cursor object
+    '''
